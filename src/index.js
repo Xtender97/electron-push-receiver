b28e1754cedccb88e89870f140fdcbd2dbcef068
Milan Boskovic <jovbosko1@gmail.com>
2024-06-18 21:26:39 +0200

    used push-receiver-v2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { register, listen } = require('push-receiver');
+const { register, listen } = require('push-receiver-v2');
 const { ipcMain } = require('electron');
 const Config = require('electron-config');
 const {
@@ -26,11 +26,11 @@ let started = false;
 // To be call from the main process
 function setup(webContents) {
   // Will be called by the renderer process
-  ipcMain.on(START_NOTIFICATION_SERVICE, async (_, senderId) => {
+  ipcMain.on(START_NOTIFICATION_SERVICE, async (_, fcmConfig) => {
     // Retrieve saved credentials
     let credentials = config.get('credentials');
     // Retrieve saved senderId
-    const savedSenderId = config.get('senderId');
+    const savedAppID = config.get('appID');
     if (started) {
       webContents.send(NOTIFICATION_SERVICE_STARTED, (credentials.fcm || {}).token);
       return;
@@ -40,12 +40,12 @@ function setup(webContents) {
       // Retrieve saved persistentId : avoid receiving all already received notifications on start
       const persistentIds = config.get('persistentIds') || [];
       // Register if no credentials or if senderId has changed
-      if (!credentials || savedSenderId !== senderId) {
-        credentials = await register(senderId);
+      if (!credentials || savedAppID !== fcmConfig.appID) {
+        credentials = await register(fcmConfig);
         // Save credentials for later use
         config.set('credentials', credentials);
         // Save senderId
-        config.set('senderId', senderId);
+        config.set('appID', fcmConfig.appID);
         // Notify the renderer process that the FCM token has changed
         webContents.send(TOKEN_UPDATED, credentials.fcm.token);
       }
@@ -69,7 +69,7 @@ function onNotification(webContents) {
     config.set('persistentIds', [...persistentIds, persistentId]);
     // Notify the renderer process that a new notification has been received
     // And check if window is not destroyed for darwin Apps
-    if(!webContents.isDestroyed()){
+    if (!webContents.isDestroyed()) {
       webContents.send(NOTIFICATION_RECEIVED, notification);
     }
   };
